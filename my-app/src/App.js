96619ca9fa2ab690389a90d42e240e3d0cb7d87d
subhradeep1007 <subhradeep1007@gmail.com>
2021-07-29 17:37:52 +0530

    First Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="">
+         Maa chudalo black mein
+      </div>
+      Maa chudao
+    </div>
+  );
+}
+
+export default App;
